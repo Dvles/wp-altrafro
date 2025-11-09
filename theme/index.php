07cda824df6227353a9982385d4373ac7b50e404
Dvles <132080646+Dvles@users.noreply.github.com>
2025-11-09 02:54:30 +0100

    fix(responsive): fix structure across breakpoints

--- theme/index.php
+++ theme/index.php
@@ -1,7 +1,7 @@
 <?php get_header(); ?>
 <!-- FEATURED IMAGE  -->
-<section class="h-screen-80 page-grid bg-slate-500 z-30 flex  items-center">
-  <div class="col-start-2 col-end-6 max-w-70">
+<section class="h-screen-80 page-grid bg-slate-500 z-30 flex  pb-20   items-end lg:pb-0 lg:items-center">
+  <div class="col-start-3 col-end-11  lg:col-start-2 lg:col-end-6">
     <?php
     get_template_part('template-parts/cards/card-featured');
     ?>
--- theme/template-parts/layouts/page-header.php
+++ theme/template-parts/layouts/page-header.php
@@ -14,14 +14,15 @@ $subheading = $args['subheading'] ?? 'subheading';
 
 
 <section class="min-h-screen-95px page-grid ">
-  <div class="col-start-2 col-end-9 global-padding bg-white z-20">
+  <div class="col-start-3 col-end-11  lg:col-start-2 lg:col-end-9 global-padding bg-white z-20">
     <h2 class="page-title"><?php echo esc_html($heading); ?></h2>
     <!-- REPLACE BY CAT FILTER MENU -->
     <div class="filter-menu ">
       <a href="#" class="filter-menu-items">.ART</a>
-      <a href="#" class="filter-menu-items">.ART</a>
-      <a href="#" class="filter-menu-items">.ART</a>
-      <a href="#" class="filter-menu-items">.ART</a>
+      <a href="#" class="filter-menu-items">.FASHION</a>
+      <a href="#" class="filter-menu-items">.INNOVATION</a>
+      <a href="#" class="filter-menu-items">.MUSIC</a>
+      <a href="#" class="filter-menu-items">.VISUAL MEDIA</a>
     </div>
   </div>
 </section>
